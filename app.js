db719c81f5f3287243bf54fce8fdc128a12cc596
lucasuezima <enteckgames@gmail.com>
2020-06-14 18:38:41 -0300

    Cadastro/Login

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ var bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     seedDB = require("./seeds"),
     User = require("./models/user"),
-    Post = require("./models/post");
+    Post = require("./models/post"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+    app.use(bodyParser.urlencoded({extended: true}));
+
 InitiateMongoServer = require("./config/db");
 // seedDB();
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Não entendi",
+    resave: false,
+    saveUninitialized: false
+}));
 
 //Iniciar o server mongo
 InitiateMongoServer();
@@ -21,21 +33,28 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // app.use(userRouter);
 
 app.get("/", function (req, res) {
     res.render("home");
 });
 
-var mongoose = require("mongoose");
 // var shortid = require("shortid");
+
 
 
 
 //RESTFUL Routes
 
 //=================== INDEX ROUTE ======================
-app.get("/profile", function (req, res) {
+app.get("/profile", isLoggedIn, function (req, res) {
     User.find({}, function (err, user) {
         if (err) {
             console.log("Erro ao carregar dados do usuário");
@@ -238,7 +257,49 @@ app.delete("/profile/:email", function (req, res) {
     // })
 });
 
+//================Cadastro====================
 
+app.get("/cadastro", function(req, res){
+    res.render("Registrar");
+});
+app.post("/cadastro", function(req, res){
+    req.body.username
+    req.body.passport
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("cadastro");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/home");
+        })
+    });
+
+});
+//================Login====================
+
+app.get("/home", function(req, res){
+    res.render("home");
+});
+
+app.post("/home", passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/home"
+}) ,function(req, res){
+
+});
+
+app.get("/logout", function (req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/home");
+};
 
 
 
--- models/user.js
+++ models/user.js
@@ -1,15 +1,16 @@
 var mongoose = require('mongoose');
+var passportLocalMongoose = require("passport-local-mongoose");
 
-var userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+var UserSchema = new mongoose.Schema({
+    name: { type: String, required: false },
     image: {type: String, default: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/512px-Circle-icons-profile.svg.png"},
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    age: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    course: { type: String, required: true },
-    university: { type: String, required: true },
+    email: { type: String, required: false },
+    password: { type: String, required: false },
+    age: { type: String, required: false },
+    city: { type: String, required: false },
+    state: { type: String, required: false },
+    course: { type: String, required: false },
+    university: { type: String, required: false },
     created: { type: Date, default: Date.now },
     // profile_pic:{type:String, default:"default_profile.png"},
     // member_id: {type: String, default: shortid.generate},
@@ -17,5 +18,6 @@ var userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref:"post"}]
 });
+UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
